146a45d562d7dd833bed92104f1a57ee9cf7d387
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-29 21:47:29 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
 });
